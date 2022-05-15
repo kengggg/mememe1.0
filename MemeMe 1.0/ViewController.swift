3b2cc86c09fe80de673c2acc00b1fab6996d6ebf
Keng Susumpow <patipat@me.com>
2022-05-16 05:02:13 +0700

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Keng Susumpow on 16/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
