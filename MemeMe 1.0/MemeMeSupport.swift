3df15b9e9a862abf58f2ed8ac0981b17a4b52750
Keng Susumpow <patipat@me.com>
2022-05-18 05:54:43 +0700

    move Meme struct to the MemeMeSupport file

---
+++ MemeMe 1.0/MemeMeSupport.swift
@@ -0,0 +1,16 @@
+//
+//  MemeMeSupport.swift
+//  MemeMe 1.0
+//
+//  Created by Keng Susumpow on 18/5/22.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
